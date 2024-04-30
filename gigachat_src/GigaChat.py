3e0a5e28b4dde08ba34ec12fdd89a6b3ee620291
Zloy-tarakaniscshe <Zloy-medved@mail.ru>
2024-04-30 21:47:16 +0800

    rename directory

--- gigachat_src/GigaChat.py
+++ gigachat_src/GigaChat.py
@@ -5,7 +5,7 @@ import shutil
 
 import config
 from log_to_mail import MailExecutor
-from GigaChat_api.functions.request_api import *
+from gigachat_src.functions.request_api import *
 
 
 class GigaChat:
